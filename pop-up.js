95ab786d3bd66bc300b53f3b551dc6791e9a4454
Redlemon8 <122209875+Redlemon8@users.noreply.github.com>
2023-08-18 09:24:51 +0200

    Update pop-up.js

--- pop-up.js
+++ pop-up.js
@@ -3,9 +3,9 @@ const popupContainer = document.getElementById('popupContainer');
 const closePopup = document.getElementById('closePopup');
 
 openPopupLink.addEventListener('click', function() {
-    popupContainer.style.display = 'block'; // Affiche la pop-up
+    popupContainer.style.display = 'block'; 
 });
 
 closePopup.addEventListener('click', function() {
-    popupContainer.style.display = 'none'; // Masque la pop-up
+    popupContainer.style.display = 'none';
 });
